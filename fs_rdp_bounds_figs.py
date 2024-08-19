c6634eaae7ca4ade1b7925f5334cdedab2eb2aeb
Mohammadreza Ebrahimi <mohammadrezaebrahimi@users.noreply.github.com>
2024-08-19 15:17:27 -0400

    Update fs_rdp_bounds_figs.py

--- fs_rdp_bounds_figs.py
+++ fs_rdp_bounds_figs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 12:15:21 2024
 
+"""
 ###Test of fixed-size subsampling (with and without replacement) Renyi Differential Privacy (FS-RDP) methods from the paper
-##Renyi DP-SGD with Fixed-Size Minibatches: Tighter Guarantees with or without Replacement 
-
+#Differentially Private Stochastic Gradient Descent with Fixed-Size Minibatches: Tighter RDP Guarantees with or without Replacement
+import sys
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from FS_RDP_bounds import FS_RDP_woR, FS_RDP_woR_convexity_method, FS_RDP_wR, FS_RDP_wR_LB_approximate, FS_RDP_wR_LB_exact
+from FS_RDP_bounds import FSwoR_RDP_ro, Poisson_RDP_ro, FSwoR_RDP_ar, FSwR_RDP_ar, FSwR_RDP_ar_LB_approximate, FSwR_RDP_ar_LB_exact
 
 
 ##Implementation of bounds from the paper Renyi differential privacy and analytical moments accountant by Wang et al, for comparison purposes
@@ -55,11 +58,12 @@ def Wang_et_al_lower_bound(alpha,sigma,q):
         print("Error, alpha must be an integer.")
         
         
-##########Comparison of our FS-RDP without replacement upper bound with results from Wang et al
+##########Comparison of our FS-RDP without replacement replace-one adjacency upper bound with results from Wang et al
 # #make plots: large alpha 
-N_alpha=500
-alpha_array=1+10**np.linspace(-2,1.5,N_alpha)
-m_array=[3,4,6,8,10]
+N_alpha=100
+alpha_array=1+10**np.linspace(-2,1.8,N_alpha)
+m_array=[3,4,5]
+
 
 B=120
 D=50000
@@ -73,23 +77,21 @@ q=B/D
 N_m=len(m_array)
 
 
-FS_eps_array=np.zeros((N_m,N_alpha))
+FS_ro_eps_array=np.zeros((N_m,N_alpha))
 
 for j1 in range(N_m):
     for j2 in range(N_alpha):
-        FS_eps_array[j1,j2]=FS_RDP_woR(alpha_array[j2],sigma,m_array[j1],q)
-
+        FS_ro_eps_array[j1,j2]=FSwoR_RDP_ro(alpha_array[j2],sigma,m_array[j1],q)
 
 integer_alpha_method_array=np.zeros(N_alpha)       
 Wang_eps_array=np.zeros(N_alpha)
 for j1 in range(N_alpha):
-    integer_alpha_method_array[j1]=FS_RDP_woR_convexity_method(alpha_array[j1],sigma,q)
     Wang_eps_array[j1]=Wang_et_al_upper_bound(alpha_array[j1],sigma,q)
     
     
 alpha_lb_array=[]
 Wang_eps_lb_array=[]
-for alpha in range(2,int(max(alpha_array))+1):
+for alpha in range(int(np.ceil(min(alpha_array))),int(max(alpha_array))+1):
     alpha_lb_array.append(alpha)
     Wang_eps_lb_array.append(Wang_et_al_lower_bound(alpha,sigma,q))
 
@@ -101,30 +103,30 @@ N_iters=1.
 plt.figure()
 labels=[]
 for k in range(N_m):
-    plt.semilogy(alpha_array,N_iters*FS_eps_array[k,:])
-    labels.append("Thm.3.3 upper bound (m = {})".format(m_array[k]))
+    plt.semilogy(alpha_array,N_iters*FS_ro_eps_array[k,:])
+    labels.append("Thm.3.4 upper bound (m = {})".format(m_array[k]))
     
-m_idx=1
-# plt.semilogy(alpha_array,N_iters*np.minimum(integer_alpha_method_array,FS_eps_array[m_idx,:]),'-',color='0')
-# labels.append("Thm.3.3 and convexity technique")
+
 
 plt.semilogy(alpha_array,N_iters*Wang_eps_array,'-.')
 labels.append("Wang et al. upper bound")
 plt.semilogy(alpha_lb_array,N_iters*np.array(Wang_eps_lb_array),'.')
 labels.append("Wang et al. lower bound")
 
-plt.legend(labels)
-plt.xlabel(r'$\alpha$')
-plt.ylabel('One-step RDP Bound')
+plt.legend(labels, fontsize=12)
+plt.xlabel(r'$\alpha$', fontsize=14)
+plt.ylabel('One-step FSwoR-RDP Bound (r-o)', fontsize=14)
 
 
-plt.savefig("comp_with_Wang_et_al_no_convexity.pdf")
+plt.savefig("comp_with_Wang_et_al_ro_adjacency.pdf")
 
 
-#make plots: small alpha
-N_alpha=500
-alpha_array=1+10**np.linspace(-2,.5,N_alpha)
-m_array=[4]
+
+
+N_alpha=100
+alpha_array=np.linspace(50,60,N_alpha)
+m_array=[5]
+
 
 B=120
 D=50000
@@ -138,56 +140,105 @@ q=B/D
 N_m=len(m_array)
 
 
-
-FS_eps_array=np.zeros((N_m,N_alpha))
+FS_ro_eps_array=np.zeros((N_m,N_alpha))
+Poisson_ro_eps_array=np.zeros((N_m,N_alpha))
 
 for j1 in range(N_m):
     for j2 in range(N_alpha):
-        FS_eps_array[j1,j2]=FS_RDP_woR(alpha_array[j2],sigma,m_array[j1],q)
-        
+        FS_ro_eps_array[j1,j2]=FSwoR_RDP_ro(alpha_array[j2],sigma,m_array[j1],q)
+        Poisson_ro_eps_array[j1,j2]=Poisson_RDP_ro(alpha_array[j2],sigma,m_array[j1],q)
 
 integer_alpha_method_array=np.zeros(N_alpha)       
 Wang_eps_array=np.zeros(N_alpha)
 for j1 in range(N_alpha):
-    integer_alpha_method_array[j1]=FS_RDP_woR_convexity_method(alpha_array[j1],sigma,q)
     Wang_eps_array[j1]=Wang_et_al_upper_bound(alpha_array[j1],sigma,q)
     
     
 alpha_lb_array=[]
 Wang_eps_lb_array=[]
-for alpha in range(2,int(max(alpha_array))+1):
+for alpha in range(int(np.ceil(min(alpha_array))),int(max(alpha_array))+1):
     alpha_lb_array.append(alpha)
     Wang_eps_lb_array.append(Wang_et_al_lower_bound(alpha,sigma,q))
-    
-    
-# N_iters=math.floor(D/B*N_epochs)
-N_iters=1
+
+#N_iters=math.floor(D/B*N_epochs)
+N_iters=1.
+
+
+
 
 plt.figure()
 labels=[]
-plt.plot(alpha_array,N_iters*integer_alpha_method_array,'-')
-labels.append("Thm.3.1 upper bound with convexity technique")
-
+colors=['black']
 for k in range(N_m):
-    plt.plot(alpha_array,N_iters*FS_eps_array[k,:])
+    plt.plot(alpha_array,N_iters*FS_ro_eps_array[k,:],color=colors[k])
+    labels.append("FSwoR upper bound (m = {})".format(m_array[k]))
+    plt.plot(alpha_array,N_iters*Poisson_ro_eps_array[k,:],'--',color=colors[k])
+    labels.append("Poisson upper bound (m = {})".format(m_array[k]))
+    
+plt.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+plt.plot(alpha_array,N_iters*Wang_eps_array,'-.',color='red')
+labels.append("Wang et al. upper bound")
+plt.plot(alpha_lb_array,N_iters*np.array(Wang_eps_lb_array),'.',color='magenta')
+labels.append("Wang et al. lower bound")
+
+
+plt.legend(labels, fontsize=12)
+plt.xlabel(r'$\alpha$', fontsize=14)
+plt.ylabel('One-step FSwoR-RDP Bound (r-o)', fontsize=14)
+
+
+plt.savefig("comp_with_Poisson_ro_adjacency.pdf")
+
+
+        
+##########Comparison of our FS-RDP without replacement add/remove adjacency upper bounds
+# #make plots: large alpha 
+N_alpha=100
+alpha_array=1+10**np.linspace(-2,1.8,N_alpha)
+m_array=[3,4]
+
+B=120
+D=50000
+N_epochs=250
+
+sigma=6.
+
+
+
+q=B/D
+N_m=len(m_array)
+
+
+FS_eps_array=np.zeros((N_m,N_alpha))
+
+for j1 in range(N_m):
+    for j2 in range(N_alpha):
+        FS_eps_array[j1,j2]=FSwoR_RDP_ar(alpha_array[j2],sigma,m_array[j1],q)
+
+
+
+
+#Wang et al not shown since those theorems assume add/remove adjacency
+plt.figure()
+labels=[]
+for k in range(N_m):
+    plt.semilogy(alpha_array,N_iters*FS_eps_array[k,:])
     labels.append("Thm.3.3 upper bound (m = {})".format(m_array[k]))
     
 
 
-plt.plot(alpha_lb_array,N_iters*np.array(Wang_eps_lb_array),'o')
-labels.append("Wang et al. lower bound")
 
-plt.legend(labels)
-plt.xlabel(r'$\alpha$')
-plt.ylabel('One-step RDP Bound')
+plt.legend(labels, fontsize=12)
+plt.xlabel(r'$\alpha$', fontsize=14)
+plt.ylabel('One-step FSwoR-RDP Bound (a/r)', fontsize=14)
 
-plt.savefig("comp_with_Wang_et_al_small_alpha.pdf")
 
+plt.savefig("FSwoR_ar_1step.pdf")
 
 
 
 
-##########Exploration of our FS-RDP with replacement upper and lower bounds
+##########Exploration of our fixed-size subsampling with replacement RDP upper and lower bounds (under add/remove adjacency)
 # #make plots: small alpha 
 alpha_array=[j for j in range(2,11)]
 for alpha in np.linspace(1.001,10,500):
@@ -195,13 +246,13 @@ for alpha in np.linspace(1.001,10,500):
 alpha_array=np.sort(np.array(alpha_array))
 N_alpha=len(alpha_array)
 
-m=8
+m=4
 
 FSR_eps_array=np.zeros(N_alpha)
 FS_eps_array=np.zeros(N_alpha)
 for j in range(N_alpha):
-    FSR_eps_array[j]=FS_RDP_wR(alpha_array[j],sigma,m,B,D)
-    FS_eps_array[j]=FS_RDP_woR(alpha_array[j],sigma,m,q)
+    FSR_eps_array[j]=FSwR_RDP_ar(alpha_array[j],sigma,m,B,D)
+    FS_eps_array[j]=FSwoR_RDP_ar(alpha_array[j],sigma,m,q)
         
 
 
@@ -214,7 +265,7 @@ for j in range(len(alpha_lb_array)):
     alpha=alpha_lb_array[j]
     
 
-    FSR_eps_lb_general_array[j]=FS_RDP_wR_LB_approximate(alpha,sigma,B,D,lambda k: indices if k >2 else np.array(range(0,B+1)))
+    FSR_eps_lb_general_array[j]=FSwR_RDP_ar_LB_approximate(alpha,sigma,B,D,lambda k: indices if k >2 else np.array(range(0,B+1)))
 
 
  
@@ -237,15 +288,15 @@ plt.semilogy(alpha_array,N_iters*np.array(FS_eps_array),'--')
 labels.append('$FS_{woR}$-RDP upper bound')
 
 
-plt.legend(labels)
-plt.xlabel(r'$\alpha$')
-plt.ylabel('One-step RDP Bound')
+plt.legend(labels, fontsize=12)
+plt.xlabel(r'$\alpha$', fontsize=14)
+plt.ylabel('One-step RDP Bound', fontsize=14)
 plt.savefig("FSR_upper_lower_bounds.pdf")
 
 
 
     
-#exact one step FS_{wR}-RDP bound as a function of B for fixed q
+#exact one step FS_{wR_ar}-RDP bound as a function of B for fixed q
 
 N_sigma=5
 sigma_array=np.linspace(2,6,N_sigma)
@@ -264,7 +315,7 @@ for B in range (B_i,B_f+1):
     D=B/q
     for j in range(N_sigma):
 
-        FSR_eps_B_array[B-B_i,j]=FS_RDP_wR_LB_exact(alpha,sigma_array[j],B,D)
+        FSR_eps_B_array[B-B_i,j]=FSwR_RDP_ar_LB_exact(alpha,sigma_array[j],B,D)
     
 plt.figure()
 labels=[]
@@ -272,9 +323,9 @@ for k in range(N_sigma):
     plt.semilogy(B_array,FSR_eps_B_array[:,k])
     labels.append(r"$\sigma={}$".format(sigma_array[k]))
     
-plt.legend(labels, loc='center right')
-plt.xlabel(r'$|B|$')
-plt.ylabel('One-step RDP Lower Bound')
+plt.legend(labels, loc='center right', fontsize=12)
+plt.xlabel(r'$|B|$', fontsize=14)
+plt.ylabel('One-step RDP Lower Bound', fontsize=14)
     
 plt.savefig("FSR_exact_vs_B.pdf")
 
@@ -325,7 +376,7 @@ for j1 in range(len(B_array)):
                 
         print("B={}, alpha={}".format(B,alpha))
 
-        eps_lb_array[j2,j1]=FS_RDP_wR_LB_approximate(alpha,sigma,B,D,lb_indices)
+        eps_lb_array[j2,j1]=FSwR_RDP_ar_LB_approximate(alpha,sigma,B,D,lb_indices)
 
     
 plt.figure()
@@ -334,9 +385,9 @@ for k in range(len(B_array)):
     plt.semilogy(alpha_lb_array,eps_lb_array[:,k])
     labels.append(r"$|B|={}$".format(B_array[k]))
     
-plt.legend(labels,loc='lower right')
-plt.xlabel(r'$\alpha$')
-plt.ylabel('One-step RDP Lower Bound')
+plt.legend(labels,loc='lower right', fontsize=12)
+plt.xlabel(r'$\alpha$', fontsize=14)
+plt.ylabel('One-step RDP Lower Bound', fontsize=14)
     
 plt.savefig("FSR_LB_vs_alpha.pdf")
 
